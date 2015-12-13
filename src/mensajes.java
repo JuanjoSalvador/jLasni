49b504df0fca66539d17763727ff6cbc5ccf483f
Juanjo Salvador <juanjosalvador@openmailbox.org>
2015-12-13 18:34:00 +0100

    src added

---
+++ src/mensajes.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author Juan José Salvador
+ */
+public class mensajes {
+    
+    public void roto() {
+        System.out.println("Hola me he roto :D");
+    }
+    
+    public void pechote() {
+        System.out.println("Me pica un pechote (????)");
+    }
+    
+    public void meIgnora(String nombre) {
+        System.out.println(nombre + " me ignora (????)");
+    }
+    
+    public void botonaco() {
+        System.out.println("Aquí va un BOTONACO");
+    }
+    
+    public void guapisimo() {
+        System.out.println("Guapísimo xDDDDDDDDDDDDD");
+    }
+    
+    // Añade más frases
+}
