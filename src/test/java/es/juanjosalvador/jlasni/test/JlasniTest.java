21dee1f08ec4d1f14142b6e10c2f5c4eee21f3b2
Victor <victor.suarez@auratecnologica.com>
2016-05-10 17:58:17 +0100

    Mavenizando del proyecto y añadidos tests

---
+++ src/test/java/es/juanjosalvador/jlasni/test/JlasniTest.java
@@ -0,0 +1,64 @@
+package es.juanjosalvador.jlasni.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import es.juanjosalvador.jlassni.JLasni;
+
+public class JlasniTest {
+
+	private static JLasni jlasni;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		jlasni=new JLasni();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testRoto() {
+		jlasni.roto();
+	}
+
+	@Test
+	public void testPechote() {
+		jlasni.pechote();
+	}
+
+	@Test
+	public void testMeIgnora() {
+		jlasni.meIgnora("@FreakViking");
+	}
+
+	@Test
+	public void testBotonaco() {
+		jlasni.botonaco();
+	}
+
+	@Test
+	public void testGuapisimo() {
+		jlasni.guapisimo();
+	}
+
+	@Test
+	public void testFelicidad() {
+		jlasni.felicidad();
+	}
+
+	@Test
+	public void testAPastar() {
+		jlasni.aPastar();
+	}
+
+	@Test
+	public void testCroqueta() {
+		jlasni.croqueta();
+	}
+
+}
